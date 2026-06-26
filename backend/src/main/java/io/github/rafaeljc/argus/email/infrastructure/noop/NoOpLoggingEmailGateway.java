fffac4e6b13e1b8fe8dfc38366b18a4ad4099356
Rafael Clemente <rafaeljc@users.noreply.github.com>
2026-06-26 12:09:28 -0300

    refactor: make NoOpLoggingEmailGateway the default EmailGateway

--- backend/src/main/java/io/github/rafaeljc/argus/email/infrastructure/noop/NoOpLoggingEmailGateway.java
+++ backend/src/main/java/io/github/rafaeljc/argus/email/infrastructure/noop/NoOpLoggingEmailGateway.java
@@ -5,11 +5,9 @@ import io.github.rafaeljc.argus.email.application.port.EmailGateway;
 import io.github.rafaeljc.argus.email.domain.OutboxMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile({"local", "test"})
 public class NoOpLoggingEmailGateway implements EmailGateway {
 
     private static final Logger log = LoggerFactory.getLogger(NoOpLoggingEmailGateway.class);
